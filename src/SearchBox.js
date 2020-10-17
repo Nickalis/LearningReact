999f400ae7541853fe76081a7f4c40fec1584974
Nickalis <nickalis1999@gmail.com>
2020-10-17 10:53:23 -0700

    React

--- src/SearchBox.js
+++ src/SearchBox.js
@@ -3,7 +3,7 @@ import React from 'react';
 const SearchBox = () => {
     return (
         <div className='pa2'>
-            <input className='pa3 ba b--green bg-lightest-blue' type="search" placeholder='search robots'/>
+            <input className='pa3 ba b--green bg-lightest-blue' type="search" placeholder='Search Robots'/>
         </div>
     );
 }
